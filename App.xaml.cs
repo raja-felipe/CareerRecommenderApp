cdfa4e6ac1f32a5b58e7ca45c3324df3207b3630
Raja Felipe <ramonjaviervi@gmail.com>
2024-08-02 18:13:07 +1000

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CareerRecommenderApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
