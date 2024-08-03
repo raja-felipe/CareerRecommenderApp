b5b584fc587acf751fd2a596eb0a7af164bae237
Raja Felipe <ramonjaviervi@gmail.com>
2024-08-03 16:16:20 +1000

    Misc C# changes

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/CareerRecommenderApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/CareerRecommenderApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+17ebef3592b026a676adfad8ce1a3b40315246c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+705d9576964984c35a0a60771802f6b741c02070")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
