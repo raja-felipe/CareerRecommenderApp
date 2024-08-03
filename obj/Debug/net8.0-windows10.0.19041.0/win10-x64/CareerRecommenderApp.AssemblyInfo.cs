a98ea68b7e13984cc461f10d92b3e753dbf465b7
Raja Felipe <ramonjaviervi@gmail.com>
2024-08-03 17:05:41 +1000

    C# misc files

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5b584fc587acf751fd2a596eb0a7af164bae237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+121f8747854287434ac58df44b87ef0142db294d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
