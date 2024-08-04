35ba3e20dbc154dea4dd7b8db8f267a7367af6b5
finnclaridge <s4014612@student.rmit.edu.au>
2024-08-04 12:29:55 +1000

    vm

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6017af1a7ee2d0236a2fc8019243bf9728267ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+439a2718f6b2cf6d5ead277a2e957cbdb642e14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
