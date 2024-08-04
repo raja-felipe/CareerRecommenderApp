0459f08c1249989090f0256638d756cb2aedeebd
finnclaridge <s4014612@student.rmit.edu.au>
2024-08-04 11:05:02 +1000

    debug

--- CompanyPage.xaml.cs
+++ CompanyPage.xaml.cs
@@ -1,9 +1,12 @@
-namespace CareerRecommenderApp;
-
-public partial class CompanyPage : ContentPage
+namespace CareerRecommenderApp
 {
-	public CompanyPage()
-	{
-		InitializeComponent();
-	}
-}
+    public partial class CompanyPage : ContentPage
+    {
+        int count = 0;
+
+        public CompanyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}  
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CareerRecommenderApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5bb7676d22347870e053467c2356a77bd0e7475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e66c5ec975c8320a2dbf9135ae13d3253225eaa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerRecommenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
